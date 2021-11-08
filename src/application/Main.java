0d0cd31905dc0bde80e7b0b403e8ed511ce52869
Zack Umar <zackumar2@gmail.com>
2021-11-08 13:22:35 -0600

    Changed project structure

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,11 @@
 package application;
 	
+import java.io.File;
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import sun.misc.UCEncoder;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
@@ -11,9 +15,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			URL url = new File("src/application/view/Main.fxml").toURI().toURL();
+			AnchorPane root = (AnchorPane)FXMLLoader.load(url);
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
