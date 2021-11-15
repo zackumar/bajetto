14a551454f66eece5f2f44360b9ad32aabba0068
Zack Umar <zackumar2@gmail.com>
2021-11-15 14:23:54 -0600

    Can go back now.

--- src/application/controller/CreateBudgetController.java
+++ src/application/controller/CreateBudgetController.java
@@ -1,10 +1,19 @@
 package application.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class CreateBudgetController {
 
@@ -30,8 +39,13 @@ public class CreateBudgetController {
     private Button btnBack;
 
     @FXML
-    void back(ActionEvent event) {
-
+    void back(ActionEvent event) throws IOException {
+    	URL url = new File("src/application/view/Main.fxml").toURI().toURL();
+    	AnchorPane mainPane = FXMLLoader.load(url);
+    	Scene scene = new Scene(mainPane);
+    	Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    	window.setScene(scene);
+    	window.show();
     }
 
     @FXML
