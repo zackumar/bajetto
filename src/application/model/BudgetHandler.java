ceae7284d4e85c59296b93def89e52de389497cf
Zack Umar <zackumar2@gmail.com>
2021-11-19 14:16:17 -0600

    Update BudgetHandler.java

--- src/application/model/BudgetHandler.java
+++ src/application/model/BudgetHandler.java
@@ -3,14 +3,35 @@ package application.model;
 import java.util.ArrayList;
 
 public class BudgetHandler {
+
+	public static final BudgetHandler INSTANCE = new BudgetHandler();
+	public static BudgetHandler getInstance() {
+		return INSTANCE;
+	}
+	
 	public String name;
 	public ArrayList<BudgetItem> items;
 	public Income income;
-	
-	public BudgetHandler(String name, Income income){
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
 		this.name = name;
-		this.items = new ArrayList<BudgetItem>();
+	}
+
+	public ArrayList<BudgetItem> getItems() {
+		return items;
+	}
+
+	public Income getIncome() {
+		return income;
+	}
+
+	public void setIncome(Income income) {
 		this.income = income;
 	}
+
 
 }
