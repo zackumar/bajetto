cfc7a8ee25763a3dfa8fc89990701b9043752210
Zack Umar <zackumar2@gmail.com>
2021-11-22 14:23:02 -0600

    Info Controller

--- src/application/controller/InfoController.java
+++ src/application/controller/InfoController.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
+import application.model.BudgetHandler;
+import application.model.BudgetItem;
+import application.model.Income.BudgetFreq;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -19,81 +22,108 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-
 public class InfoController {
 
-    @FXML
-    private PieChart pieSpendings;
+	@FXML
+	private Text txtSpent;
 
-    @FXML
-    private TextArea fieldTransaction;
+	@FXML
+	private Text txtFreq;
 
-    @FXML
-    private Button btnAddOneTime;
+	@FXML
+	private PieChart pieSpendings;
 
-    @FXML
-    private Text textName;
+	@FXML
+	private TextArea fieldTransaction;
 
-    @FXML
-    private TextArea fieldTransaction1;
+	@FXML
+	private Button btnAddOneTime;
 
-    @FXML
-    private Button btnAddRecurring;
-    
-    @FXML 
-    void initialize() {
-    	ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList( 
-    			   new PieChart.Data("RENT", 500), 
-    			   new PieChart.Data("NET", 25), 
-    			   new PieChart.Data("MOTO G", 10), 
-    			   new PieChart.Data("Nokia Lumia", 22)); 
-    	
-    	pieSpendings.setData(pieChartData);
-    	
-    	
-    }
-    
+	@FXML
+	private Text textName;
 
-    @FXML
-    void addOneTime(ActionEvent event) throws IOException {
-    	URL url = new File("src/application/view/ExpensesView.fxml").toURI().toURL();
-    	FXMLLoader loader = new FXMLLoader();
-    	loader.setLocation(url);
-    	
-  
-    	ExpensesController controller = new ExpensesController(true);
-    	loader.setController(controller);
-  
-    	AnchorPane mainPane = loader.load();
-    	
-    	Scene scene = new Scene(mainPane);
-    	
-    	Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    	window.setScene(scene);
-    	window.show();
+	@FXML
+	private TextArea fieldTransaction1;
 
-    }
+	@FXML
+	private Button btnAddRecurring;
 
-    @FXML
-    void addRecurring(ActionEvent event) throws IOException {
-    	URL url = new File("src/application/view/ExpensesView.fxml").toURI().toURL();
-    	FXMLLoader loader = new FXMLLoader();
-    	loader.setLocation(url);
-    	
-  
-    	ExpensesController controller = new ExpensesController();
-    	loader.setController(controller);
-  
-    	AnchorPane mainPane = loader.load();
-    	
-    	Scene scene = new Scene(mainPane);
-    	
-    	Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    	window.setScene(scene);
-    	window.show();
+	@FXML
+	private Text txtIncome;
 
-    }
+	@FXML
+	void initialize() {
+
+		BudgetHandler handler = BudgetHandler.getInstance();
+		textName.setText(handler.getName());
+		txtIncome.setText(String.format("%.2f",handler.income.income));
+		txtFreq.setText(handler.income.freq + "");
+
+		double reccuring = 0;
+		double onetime = 0;
+		double total = 0;
+
+		for (BudgetItem item : handler.getItems()) {
+			total += item.price;
+
+			if (item.freq == BudgetFreq.ONETIME) {
+				onetime += item.price;
+				fieldTransaction.appendText(item.name + " - $" + String.format("%.2f", item.price) + "\n");
+			} else {
+				reccuring += item.price;
+				fieldTransaction1.appendText(item.name + " - $" + String.format("%.2f", item.price) + "\n");
+			}
+		}
+
+		txtSpent.setText(total + "");
+
+		ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+				new PieChart.Data("Recurring - %" + (int) ((reccuring / handler.income.income) * 100), reccuring),
+				new PieChart.Data("One-Time - %" + (int) ((onetime / handler.income.income) * 100), onetime),
+				new PieChart.Data(
+						"Remaining - %" + (int) (((handler.income.income - total) / handler.income.income) * 100),
+						handler.income.income - total));
+
+		pieSpendings.setData(pieChartData);
+
+	}
+
+	@FXML
+	void addOneTime(ActionEvent event) throws IOException {
+		URL url = new File("src/application/view/ExpensesView.fxml").toURI().toURL();
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(url);
+
+		ExpensesController controller = new ExpensesController(true);
+		loader.setController(controller);
+
+		AnchorPane mainPane = loader.load();
+
+		Scene scene = new Scene(mainPane);
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+
+	}
+
+	@FXML
+	void addRecurring(ActionEvent event) throws IOException {
+		URL url = new File("src/application/view/ExpensesView.fxml").toURI().toURL();
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(url);
+
+		ExpensesController controller = new ExpensesController();
+		loader.setController(controller);
+
+		AnchorPane mainPane = loader.load();
+
+		Scene scene = new Scene(mainPane);
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+
+	}
 
 }
-
-
